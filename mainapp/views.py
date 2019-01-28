519a6854dfae174f3878705cd40c55b165a73c55
SashnikovNA <kapodastr@inbox.ru>
2019-01-29 00:41:54 +0300

    в рамках 3го ДЗ добавления

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,13 +5,14 @@ from .models import ProductCategory, Product
 
 # Create your views here.
 def index(request: HttpRequest):
-    title = 'главная'
     products = Product.objects.all()[:4]
-    content = {'title': title, 'products': products}
+    content = {'products': products}
     return render(request, 'mainapp/index.html', content)
 
 def products(request: HttpRequest):
-    return render(request, 'mainapp/products.html')
+    products = Product.objects.all()[:4]
+    content = {'products': products}
+    return render(request, 'mainapp/products.html', content)
 
 def contact(request: HttpRequest):
     return render(request, 'mainapp/contact.html')
